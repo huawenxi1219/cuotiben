932618dabe2b9d43b851ab1c91794f13cdea7944
huawenxi1219 <2142796912@qq.com>
2026-07-31 16:56:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
 import flet as ft
-import os
 
 def main(page: ft.Page):
     page.title = "测试"
     page.add(ft.Text("✅ 成功启动！", size=30))
     page.update()
-    # 写入成功标记
-    try:
-        with open("/sdcard/success.txt", "w") as f:
-            f.write("App started successfully")
-    except:
-        pass
 
 if __name__ == "__main__":
     ft.app(target=main)
